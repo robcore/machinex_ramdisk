9eafa82b7e85ba7bdd85d54f248bbb97be6f4068
robcore <robpatershuk@gmail.com>
2017-08-05 00:26:26 -0600

    updates from the last couple months

--- res/synapse/files/.device.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-
-UKM=/data/.machinex;
-BB=busybox
-DEVICE=`/res/synapse/files/gs4.sh`;
-$BB echo "$DEVICE";
--- sbin/quicklinker.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-export PATH=/sbin:/system/bin:/system/xbin:/res/synapse/actions:$PATH
-
-# Symlink init to daemons
-ln -sf /init /sbin/ueventd
-ln -sf /init /sbin/watchdogd
-
